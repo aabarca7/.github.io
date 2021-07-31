e906527b0418a171f062a7b8173b7bc7c0e0f90c
aabarca7 <84866480+aabarca7@users.noreply.github.com>
2021-07-30 22:26:15 -0500

    fix(estetica-funcionalidad)

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # .github.io
 Proyecto 2: Aplicación CRUD
+Que tal este es Proyecto No. 2: Desarrollo de Software CRUD
+
+Mi nombre es Alan Abarca y este es mi proyecto el cual es una página realizada con boostrap.
+
+La página está estructurada así:
+-Header sección de título.
+-Descripción donde se indica que es un CRUD
+-formulario para insertar tareas y descripción
+-sección para leer las tareas con botón de actualizar y eliminar.
+
+Para entrar a la página se puede acceder mediante el siguiente link:
+https://aabarca7.github.io./
+
+Notas: Se intentó hacer parecido al demo de los maestros para tomar como reto hacer una interfaz parecida.
+Algunas cosas se hacen de diferente manera para utilizar todo lo aprendido en clase así como cosas que se buscaron extra.
+
--- index.js
+++ index.js
@@ -7,88 +7,97 @@ const listaActividadesUI = document.getElementById('listaActividades');
 let arrayActividades = [];
 
 //funciones
-const CrearItem = (actividad, descripcion) => {
-    //si actualiza
-    if (document.querySelector("#agregar").textContent === "Actualizar") {
-        console.log("actual")
-        arrayActividades[indexArray2].actividad = document.querySelector("#actividad").value
-        arrayActividades[indexArray2].descripcion = document.querySelector("#descripcion").value
-        GuardarDB();
-    } else {
-        //si es nuevo
-        let item = {
-            actividad: actividad,
-            descripcion: descripcion
-        }
-        arrayActividades.push(item);
-        return item;
-    }
+const CrearItem = (actividad,descripcion) => {
+   //si actualiza
+  if (document.querySelector("#agregar").textContent === "Actualizar"){
+    arrayActividades[indexArray2].actividad = document.querySelector("#actividad").value
+    arrayActividades[indexArray2].descripcion = document.querySelector("#descripcion").value
+    GuardarDB();
+    document.querySelector("#agregar").textContent="Agregar tarea"
+    document.getElementById("agregar").className += "btn btn-primary";
+    document.querySelector("#actividad").className += "form-label form-control shadow-none";
+    document.querySelector("#descripcion").className += "form-label form-control shadow-none";
+  }else{
+      //si es nuevo
+      let item = {
+        actividad: actividad,
+        descripcion: descripcion
+      }
+      arrayActividades.push(item);
+      return item;
+  }
 }
 
 const GuardarDB = () => {
-    localStorage.setItem('rutina', JSON.stringify(arrayActividades));
-    MostrarDB();
+  localStorage.setItem('rutina', JSON.stringify(arrayActividades));
+  MostrarDB();
 }
 
 const MostrarDB = () => {
-    listaActividadesUI.innerHTML = '';
-    arrayActividades = JSON.parse(localStorage.getItem('rutina'));
-    //console.log(arrayActividades);
-    if (arrayActividades === null) {
-        arrayActividades = [];
-    } else {
-        arrayActividades.forEach(element => {
-
-            listaActividadesUI.innerHTML += `<div class="alert alert-primary" role="alert"> <b>${element.actividad}</b>${element.descripcion} <span class="float-right" style="float: right;"> <button type="submit" class="btn btn-primary">Eliminar</button> <button type="submit" class="btn btn-primary">Editar</button> </span> </div>`
-
-        });
-    }
+  listaActividadesUI.innerHTML = '';
+  arrayActividades = JSON.parse(localStorage.getItem('rutina'));
+  //console.log(arrayActividades);
+  if(arrayActividades === null)
+  {
+    arrayActividades = [];
+  }
+  else
+  {
+    arrayActividades.forEach(element => {
+        listaActividadesUI.innerHTML += `<div class="alert alert-primary" role="alert"> <b>${element.actividad}</b><br>${element.descripcion} <span class="float-right" style="float: right;"> <button type="submit" class="btn btn-primary">Eliminar</button> <button type="submit" class="btn btn-primary">Editar</button> </span> </div>`
+    });
+  }
 }
 
 const EliminarDB = (actividad) => {
-    let indexArray;
-    arrayActividades.forEach((elemento, index) => {
-        if (elemento.actividad === actividad) {
-            indexArray = index;
-        }
-    });
-    arrayActividades.splice(indexArray, 1);
-    GuardarDB();
+  let indexArray;
+  arrayActividades.forEach((elemento, index) => {
+    if(elemento.actividad === actividad){
+      indexArray = index;
+    }
+  });  
+  arrayActividades.splice(indexArray,1);
+  GuardarDB();
 }
 
 const EditarDB = (actividad) => {
-    indexArray2 = arrayActividades.findIndex((elemento) => elemento.actividad === actividad);
-    document.querySelector("#actividad").value = arrayActividades[indexArray2].actividad;
-    document.querySelector("#descripcion").value = arrayActividades[indexArray2].descripcion;
-    document.querySelector("#agregar").textContent = "Actualizar"
-}
+  indexArray2 = arrayActividades.findIndex((elemento)=>elemento.actividad === actividad);
+  document.querySelector("#actividad").value = arrayActividades[indexArray2].actividad;
+  document.querySelector("#descripcion").value = arrayActividades[indexArray2].descripcion;
+  document.querySelector("#agregar").textContent="Actualizar"
 
+//  document.querySelector("#agregar").element.classList.replace('btn btn-primary', 'btn btn-warning');
+    document.getElementById("agregar").className += "btn btn-warning";
+    document.querySelector("#actividad").className += "form-label form-control shadow-lg bg-body rounded";
+  document.querySelector("#descripcion").className += "form-label form-control shadow-lg bg-body rounded";
+}
+    
 //eventos
 formularioUI.addEventListener('submit', (e) => {
-    e.preventDefault();
-    let actividadUI = document.querySelector('#actividad').value;
-    let descripcionUI = document.querySelector('#descripcion').value;
-    CrearItem(actividadUI, descripcionUI);
-    GuardarDB();
-    formularioUI.reset();
+  e.preventDefault();
+  let actividadUI = document.querySelector('#actividad').value;
+  let descripcionUI = document.querySelector('#descripcion').value;
+  CrearItem(actividadUI,descripcionUI);
+  GuardarDB();
+  formularioUI.reset();
 });
 
 ///
 document.addEventListener('DOMContentLoaded', MostrarDB);
 ////
 listaActividadesUI.addEventListener('click', (e) => {
-    e.preventDefault();
-    //let texto = e.path[2].childNodes[1].innerHTML;
-    //console.log(texto);
-    if (e.target.innerHTML === 'Editar' || e.target.innerHTML === 'Eliminar') {
-        let texto = e.path[2].childNodes[1].innerHTML;
-        //let descripcion = e.path[2].childNodes[5].innerHTML;
-        //console.log(descripcion);
-        if (e.target.innerHTML === 'Eliminar') {
-            EliminarDB(texto);
-        }
-        if (e.target.innerHTML === 'Editar') {
-            EditarDB(texto);
-        }
+  e.preventDefault();
+  //let texto = e.path[2].childNodes[1].innerHTML;
+  //console.log(texto);
+  if(e.target.innerHTML === 'Editar' || e.target.innerHTML === 'Eliminar'){
+    let texto = e.path[2].childNodes[1].innerHTML;
+    //let descripcion = e.path[2].childNodes[5].innerHTML;
+    //console.log(descripcion);
+    if(e.target.innerHTML === 'Eliminar'){
+      EliminarDB(texto);
     }
+    if(e.target.innerHTML === 'Editar'){      
+        EditarDB(texto);
+    }
+  }
 });
