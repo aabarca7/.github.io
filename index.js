510c155de3fff8274a21c7d8d6a5447c439faaf0
aabarca7 <84866480+aabarca7@users.noreply.github.com>
2021-07-30 20:49:50 -0500

    feat(eliminar)

--- index.js
+++ index.js
@@ -5,24 +5,20 @@ const listaActividadesUI = document.getElementById('listaActividades');
 let arrayActividades = [];
 
 //funciones
-
 const CrearItem = (actividad, descripcion) => {
-
     let item = {
         actividad: actividad,
         descripcion: descripcion
     }
-
     arrayActividades.push(item);
     return item;
 }
-
 const GuardarDB = () => {
     localStorage.setItem('rutina', JSON.stringify(arrayActividades));
+    MostrarDB();
+
 }
-
 const MostrarDB = () => {
-
     listaActividadesUI.innerHTML = '';
     arrayActividades = JSON.parse(localStorage.getItem('rutina'));
     //console.log(arrayActividades);
@@ -30,14 +26,21 @@ const MostrarDB = () => {
         arrayActividades = [];
     } else {
         arrayActividades.forEach(element => {
-            if (element.estado) {
-                listaActividadesUI.innerHTML += `<div class="alert alert-primary" role="alert"> <b>${element.actividad}</b>${element.descripcion} <span class="float-right" style="float: right;"> <button type="submit" class="btn btn-primary">Eliminar</button> <button type="submit" class="btn btn-primary">Editar</button> </span> </div>`
-            } else {
-                listaActividadesUI.innerHTML += `<div class="alert alert-primary" role="alert"> <b>${element.actividad}</b>${element.descripcion} <span class="float-right" style="float: right;"> <button type="submit" class="btn btn-primary">Eliminar</button> <button type="submit" class="btn btn-primary">Editar</button> </span> </div>`
-            }
+            listaActividadesUI.innerHTML += `<div class="alert alert-primary" role="alert"> <b>${element.actividad}</b>${element.descripcion} <span class="float-right" style="float: right;"> <button type="submit" class="btn btn-primary">Eliminar</button> <button type="submit" class="btn btn-primary">Editar</button> </span> </div>`
         });
-
     }
+}
+
+const EliminarDB = (actividad) => {
+    let indexArray;
+    arrayActividades.forEach((elemento, index) => {
+        if (elemento.actividad === actividad) {
+            indexArray = index;
+        }
+    });
+
+    arrayActividades.splice(indexArray, 1);
+    GuardarDB();
 }
 
 
@@ -51,6 +54,21 @@ formularioUI.addEventListener('submit', (e) => {
     GuardarDB();
     formularioUI.reset();
 });
-
-
+///
 document.addEventListener('DOMContentLoaded', MostrarDB);
+////
+listaActividadesUI.addEventListener('click', (e) => {
+    e.preventDefault();
+    //let texto = e.path[2].childNodes[1].innerHTML;
+    //console.log(texto);
+    //console.log(e.target.innerHTML);
+    if (e.target.innerHTML === 'Editar' || e.target.innerHTML === 'Eliminar') {
+        let texto = e.path[2].childNodes[1].innerHTML;
+        //console.log(texto);
+        if (e.target.innerHTML === 'Eliminar') {
+            EliminarDB(texto);
+        }
+
+    }
+
+});
